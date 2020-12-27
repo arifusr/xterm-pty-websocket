102287d8c12d8da93bfac28da92df35068af887c
arifusr <arifusr@gmail.com>
2020-12-27 10:15:42 +0700

    console.log

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ ptyProcess.on('data', function(data) {
   process.stdout.write(data);
 });
 var stdin = process.stdin;
+console.log(process)
 
 // without this, we would only get streams once enter is pressed
 stdin.setRawMode( true );
